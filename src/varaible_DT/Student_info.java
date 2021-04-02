3328213055cd27f1b27829577b771cd18b214f46
HP <HP@DESKTOP-CT939M6>
2021-04-02 20:19:13 +0530

    [sagar] variable

---
+++ src/varaible_DT/Student_info.java
@@ -0,0 +1,28 @@
+package varaible_DT;
+
+public class Student_info {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1. variable Declaration
+		int StudentRollNo;
+		String StudentName;
+		char StudentGrade;
+		float StudentPer;
+		String StudentEmailid;
+		//2. variable initilization
+		StudentRollNo=100;
+		StudentName="Sagar";
+		StudentGrade='A';
+		StudentPer=70f;
+		StudentEmailid="sagar.nagalgaon23@gmail.com";
+		//3.usage
+		System.out.println("StudentRollNo:"+StudentRollNo);
+		System.out.println("StudentNAme:"+StudentName);
+		System.out.println("Student Grade:"+StudentGrade);
+		System.out.println("Student Percentage:"+StudentPer);
+		System.out.println("Student Email_id:"+StudentEmailid);
+
+	}
+
+}
